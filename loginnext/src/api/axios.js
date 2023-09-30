7cc4a955d1843d3d269964825fd4bb6bca86b74b
codingdud <animesh384@gmail.com>
2023-10-01 02:28:52 +0530

    base url 2

--- loginnext/src/api/axios.js
+++ loginnext/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = 'https://loginapi-q785gydoq-codingdud.vercel.app';
+const BASE_URL = 'https://loginapi-h17lp1vzi-codingdud.vercel.app';
 
 export default axios.create({
     baseURL: BASE_URL,
