6b71bd9067df3bea8b9f9cbb057ca53b4033743b
codingdud <codingdud@gmail.com>
2024-06-23 06:18:14 +0530

    Changes to be committed: 	modified:   loginapi/server.js 	modified:   loginnext/src/index.css

--- loginapi/server.js
+++ loginapi/server.js
@@ -42,7 +42,7 @@ app.use(cors(corsOptions))
 
 app.use((req, res, next) => {
   // console.log(req.headers.origin)
-  res.setHeader('Access-Control-Allow-Origin', `${whitelist[whitelist.indexOf(req.headers.origin)]}`);
+  //res.setHeader('Access-Control-Allow-Origin', `${whitelist[whitelist.indexOf(req.headers.origin)]}`);
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Content-Type', 'text/html');
